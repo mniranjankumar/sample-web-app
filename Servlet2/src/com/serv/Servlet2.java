54ebd1e7b55660b97223539de2695e965039f406
niranjan <niranjan.mnk.07@gmail.com>
2018-09-30 19:15:17 +0530

    Servlet2. This is used for testing purpose created. Servlet1 & Servlet2 to transfer data.

---
+++ Servlet2/src/com/serv/Servlet2.java
@@ -0,0 +1,53 @@
+package com.serv;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doTask(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doTask(request, response);
+	}
+
+	private void doTask(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		System.out.println("Servlet2");
+		HttpSession s2 = request.getSession();
+		String fname = (String)s2.getAttribute("fname");
+		ServletContext sc = request.getServletContext();
+		System.out.println(sc.getAttribute("fname"));
+		System.out.println(fname);
+	}
+
+}
